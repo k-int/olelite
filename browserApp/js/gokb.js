635efa47b331f55e1dc9922ff56fbc664079baf2
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-24 17:28:35 +0100

    updated

--- browserApp/js/gokb.js
+++ browserApp/js/gokb.js
@@ -4,11 +4,8 @@
 (function() {
   var app = angular.module('GOKb',[ 'ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.bootstrap']);
 
-  app.controller('GOKbCtrl', function($scope,$http,ngDialog,$log) {
-    $scope.root={};
-
-    $scope.init = function(oid, modelChangeFunc, notifyRecordFunc) {
-    };
+  app.controller('GOKbCtrl', function($scope,$http) {
   });
+
 })();
 
