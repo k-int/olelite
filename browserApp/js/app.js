5f7f9c53b4efe9e291c713a2366bdf072d16c39d
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-25 11:02:19 +0100

    installed ng-grid and first example working, with data from scope

--- browserApp/js/app.js
+++ browserApp/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('OleLite', [
   'ngRoute',
   'myApp.filters',
   'myApp.services',
--- browserApp/js/gokb.js
+++ browserApp/js/gokb.js
@@ -2,9 +2,14 @@
 
 
 (function() {
-  var app = angular.module('GOKb',[ 'ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.bootstrap']);
+  var app = angular.module('GOKb',[ 'ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.bootstrap', 'ui.grid']);
 
   app.controller('GOKbCtrl', function($scope,$http) {
+
+    $scope.packageData = [
+      { 'name' : 'testPackage' }
+    ];
+
   });
 
 })();
