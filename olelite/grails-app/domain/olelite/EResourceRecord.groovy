a2b4daff0be988c78f74a09f5501e4ad4b9fb073
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-29 17:27:06 +0000

    adding domain classes

---
+++ olelite/grails-app/domain/olelite/EResourceRecord.groovy
@@ -0,0 +1,60 @@
+package olelite
+
+class EResourceRecord {
+
+  String title
+  String description
+  String isbn
+  
+  static constraints = {
+  }
+
+  static mapping = {
+    version column: 'VER_NBR'
+    id column:'E_RES_REC_ID'
+    title column:'TITLE'
+    description column:'DESCR'
+    isbn column:'ISBN', length:100
+    // | E_RES_REC_ID      | varchar(10)  | NO   | PRI |         |       |
+    // | OBJ_ID            | varchar(36)  | YES  |     | NULL    |       |
+    // | VER_NBR           | decimal(8,0) | YES  |     | NULL    |       |
+    // | TITLE             | varchar(100) | YES  |     | NULL    |       |
+    // | DESCR             | varchar(800) | YES  |     | NULL    |       |
+    // | PUBHR             | varchar(500) | YES  |     | NULL    |       |
+    // | GOKB_ID           | varchar(100) | YES  |     | NULL    |       |
+    // | ISBN              | varchar(100) | YES  |     | NULL    |       |
+    // | STAT_ID           | varchar(40)  | YES  | MUL | NULL    |       |
+    // | STAT_DT           | varchar(40)  | YES  |     | NULL    |       |
+    // | PLTFRM_PROV       | varchar(250) | YES  |     | NULL    |       |
+    // | FD_CD             | varchar(10)  | YES  |     | NULL    |       |
+    // | VNDR_NM           | varchar(40)  | YES  |     | NULL    |       |
+    // | VNDR_ID           | varchar(40)  | YES  |     | NULL    |       |
+    // | ESTD_PR           | varchar(40)  | YES  |     | NULL    |       |
+    // | ORD_TYP_ID        | decimal(8,0) | YES  |     | NULL    |       |
+    // | PYMT_TYP_ID       | varchar(10)  | YES  | MUL | NULL    |       |
+    // | PCKG_TYP_ID       | varchar(10)  | YES  | MUL | NULL    |       |
+    // | PCKG_SCP_ID       | varchar(10)  | YES  | MUL | NULL    |       |
+    // | BRKBLE            | varchar(1)   | YES  |     | NULL    |       |
+    // | FD_TITLE_LST      | varchar(1)   | YES  |     | NULL    |       |
+    // | NTE               | varchar(800) | YES  |     | NULL    |       |
+    // | PUB_DISP_NOTE     | varchar(800) | YES  |     | NULL    |       |
+    // | REQ_SEL_COMM      | varchar(800) | YES  |     | NULL    |       |
+    // | REQ_PRTY_ID       | varchar(40)  | YES  | MUL | NULL    |       |
+    // | TECH_REQ          | varchar(800) | YES  |     | NULL    |       |
+    // | ACC_TYP_ID        | varchar(40)  | YES  | MUL | NULL    |       |
+    // | NUM_SIMULT_USER   | varchar(25)  | YES  |     | NULL    |       |
+    // | AUTHCAT_TYP_ID    | varchar(40)  | YES  | MUL | NULL    |       |
+    // | STAT_SRCH_CD_ID   | decimal(8,0) | YES  | MUL | NULL    |       |
+    // | ACC_LOC_ID        | varchar(40)  | YES  |     | NULL    |       |
+    // | TRL_ND            | varchar(1)   | YES  |     | NULL    |       |
+    // | TRL_STAT          | varchar(40)  | YES  |     | NULL    |       |
+    // | LIC_ND            | varchar(1)   | YES  |     | NULL    |       |
+    // | LIC_REQ_STAT      | varchar(40)  | YES  |     | NULL    |       |
+    // | ORD_PAY_STAT      | varchar(40)  | YES  |     | NULL    |       |
+    // | ACT_STAT          | varchar(40)  | YES  |     | NULL    |       |
+    // | DEF_COVR          | varchar(100) | YES  |     | NULL    |       |
+    // | DEF_PER_ACC       | varchar(100) | YES  |     | NULL    |       |
+    // | EINST_FLAG        | varchar(1)   | YES  |     | NULL    |       |
+    // | E_RES_REC_DOC_NUM | varchar(40)  | YES  |     | NULL    |       |
+  }
+}
---
+++ olelite/grails-app/domain/olelite/EResourceRecordInstance.groovy
@@ -0,0 +1,50 @@
+package olelite
+
+class EResourceRecordInstance {
+
+  EResourceRecord parent
+
+  static constraints = {
+  }
+
+  static mapping = {
+    version column: 'VER_NBR'
+    id column:'E_RES_INS_ID'
+    parent column:'E_RES_REC_ID'
+  }
+
+    // +-------------------+--------------+------+-----+---------+-------+
+    // | Field             | Type         | Null | Key | Default | Extra |
+    // +-------------------+--------------+------+-----+---------+-------+
+    // | E_RES_INS_ID      | varchar(10)  | NO   | PRI |         |       |
+    // | OBJ_ID            | varchar(36)  | YES  |     | NULL    |       |
+    // | VER_NBR           | decimal(8,0) | YES  |     | NULL    |       |
+    // | INST_ID           | varchar(50)  | YES  |     | NULL    |       |
+    // | HOLD_ID           | varchar(50)  | YES  |     | NULL    |       |
+    // | INST_FLAG         | varchar(10)  | YES  |     | NULL    |       |
+    // | INST_NM           | varchar(500) | YES  |     | NULL    |       |
+    // | ISBN              | varchar(800) | YES  |     | NULL    |       |
+    // | INST_HOLD         | varchar(500) | YES  |     | NULL    |       |
+    // | PUB_DISP_NTE      | varchar(800) | YES  |     | NULL    |       |
+    // | PUBHR             | varchar(200) | YES  |     | NULL    |       |
+    // | PLTFRM            | varchar(100) | YES  |     | NULL    |       |
+    // | STATUS            | varchar(40)  | YES  |     | NULL    |       |
+    // | SUB_STATUS        | varchar(40)  | YES  |     | NULL    |       |
+    // | AUTO_INST_REC     | varchar(100) | YES  |     | NULL    |       |
+    // | COV_SRT_DT        | datetime     | YES  |     | NULL    |       |
+    // | COV_SRT_VOL       | varchar(40)  | YES  |     | NULL    |       |
+    // | COV_SRT_ISS       | varchar(40)  | YES  |     | NULL    |       |
+    // | COV_END_DT        | datetime     | YES  |     | NULL    |       |
+    // | COV_END_VOL       | varchar(40)  | YES  |     | NULL    |       |
+    // | COV_END_ISS       | varchar(40)  | YES  |     | NULL    |       |
+    // | PRPTL_ACC_SRT_DT  | datetime     | YES  |     | NULL    |       |
+    // | PRPTL_ACC_SRT_VOL | varchar(40)  | YES  |     | NULL    |       |
+    // | PRPTL_ACC_SRT_ISS | varchar(40)  | YES  |     | NULL    |       |
+    // | PRPTL_ACC_END_DT  | datetime     | YES  |     | NULL    |       |
+    // | PRPTL_ACC_END_VOL | varchar(40)  | YES  |     | NULL    |       |
+    // | PRPTL_ACC_END_ISS | varchar(40)  | YES  |     | NULL    |       |
+    // | E_RES_REC_ID      | varchar(10)  | YES  | MUL | NULL    |       |
+    // | BIB_ID            | varchar(40)  | YES  |     | NULL    |       |
+    // +-------------------+--------------+------+-----+---------+-------+
+    // 
+}
