27dbe7b9cb4bdc74a2e0f1a1e3142e0fe36b937a
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-11-02 17:17:55 +0000

    updated database binding

---
+++ olelite/grails-app/domain/olelite/GokbPackage.groovy
@@ -0,0 +1,25 @@
+package olelite
+
+class GokbPackage {
+
+  String id
+  String packageName
+  String packageIdentifier
+  BigDecimal version
+
+  static constraints = {
+    version(max:new BigDecimal(99999999.0),scale:0)
+  }
+
+  static mapping = {
+    table 'ole_gokb_pkg_t'
+
+    version column: 'VER_NBR'
+    // Consider --    id generator: 'hilo', params: [table: 'ole_e_res_rec_s', column: 'id', max_lo: 100]
+    // id generator:'assigned', column:'E_RES_REC_ID'
+    // seqhilo
+    id generator: 'hilo', params: [table: 'ole_gokb_pkg_s', column: 'id', max_lo: 100], column:'E_RES_REC_ID'
+    packageName column:'PKG_NAME'
+    packageIdentifier column:'PKG_IDENTIFIER'
+  }
+}
