7952701fd4f5ada0d731c61dc0d0685adfe2b9a8
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-11-07 10:49:41 -0500

    Added GokbTipp domain object

--- ole-gokb.sql
+++ ole-gokb.sql
@@ -65,3 +65,34 @@ CREATE TABLE `OLE_GOKB_PKG_S` (
 
 insert into ole_gokb_pkg_s values (0);
 
+
+CREATE TABLE OLE_GOKB_TIPP_T
+(
+      TIPP_ID INT(11) COLLATE utf8_bin NOT NULL DEFAULT 0
+        , OBJ_ID VARCHAR(36) NOT NULL
+        , VER_NBR DECIMAL(8) default 1 NOT NULL
+        , ISBN varchar(40) COLLATE utf8_bin DEFAULT NULL
+        , ISSN varchar(40) COLLATE utf8_bin DEFAULT NULL
+        , DOI varchar(100) COLLATE utf8_bin DEFAULT NULL
+        , GOKB_PKG_FK INT(11) NOT NULL
+        , ACCESS_URL varchar(800) COLLATE utf8_bin DEFAULT NULL
+        , COV_START_DATE datetime DEFAULT NULL
+        , COV_START_VOL varchar(40) COLLATE utf8_bin DEFAULT NULL
+        , COV_START_ISS varchar(40) COLLATE utf8_bin DEFAULT NULL
+        , COV_END_DATE datetime DEFAULT NULL
+        , COV_END_VOL varchar(40) COLLATE utf8_bin DEFAULT NULL
+        , COV_END_ISS varchar(40) COLLATE utf8_bin DEFAULT NULL
+    , CONSTRAINT OLE_GOKB_TIPP_TP1 PRIMARY KEY(TIPP_ID)
+    , CONSTRAINT GOKB_PKG_FK FOREIGN KEY (GOKB_PKG_FK)  REFERENCES OLE_GOKB_PKG_T ( PKG_ID )
+
+) ENGINE InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
+
+drop table if exists OLE_GOKB_TIPP_S;
+
+CREATE TABLE `OLE_GOKB_TIPP_S` (
+  `id` bigint(19) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+insert into ole_gokb_tipp_s values (0);
+
---
+++ olelite/grails-app/domain/olelite/GokbTipp.groovy
@@ -0,0 +1,36 @@
+package olelite
+
+import java.sql.Blob
+import org.hibernate.Session
+
+
+class GokbTipp {
+
+  static transients = [ 'sessionFactory' ]
+  def sessionFactory
+
+  Integer id
+  Date createdDate
+  Date lastModifiedDate
+
+  static constraints = {
+    version(max:new BigDecimal(99999999.0),scale:0)
+    createdDate(nullable:true, blank:false)
+    lastModifiedDate(nullable:true, blank:false)
+  }
+
+  static mapping = {
+    table 'ole_gokb_tipp_t'
+
+    version column: 'VER_NBR'
+    // Consider --    id generator: 'hilo', params: [table: 'ole_e_res_rec_s', column: 'id', max_lo: 100]
+    // This works OK
+    // id generator:'assigned', column:'PKG_ID'
+    // seqhilo
+    // id generator: 'hilo', params: [table: 'ole_gokb_pkg_t', column: 'PKG_ID', max_lo: 100], column:'PKG_ID'
+    id generator: 'hilo', params: [table: 'ole_gokb_tipp_s', column: 'ID', max_lo: 100], column:'TIPP_ID'
+    createdDate column:'DATE_CREATED'
+    lastModifiedDate column:'LAST_UPDATED'
+  }
+
+}
