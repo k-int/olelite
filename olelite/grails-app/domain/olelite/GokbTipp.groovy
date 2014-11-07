bcd6117fa456192222158f1de7736a3ed84eb7e6
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-11-07 11:38:07 -0500

    Added tipp-esq cols to mapping

--- olelite/grails-app/domain/olelite/GokbTipp.groovy
+++ olelite/grails-app/domain/olelite/GokbTipp.groovy
@@ -1,16 +1,22 @@
 package olelite
 
-import java.sql.Blob
-import org.hibernate.Session
-
-
 class GokbTipp {
-
-  static transients = [ 'sessionFactory' ]
-  def sessionFactory
 
   Integer id
   BigDecimal version
+  String isbn
+  String issn
+  String eissn
+  String doi
+  GokbPackage pkg
+  String accessUrl
+  Date coverageStartDate
+  String coverageStartVolume
+  String coverageStartIssue
+  Date coverageEndDate
+  String coverageEndVolume
+  String coverageEndIssue
+
   Date createdDate
   Date lastModifiedDate
 
@@ -22,14 +28,20 @@ class GokbTipp {
 
   static mapping = {
     table 'ole_gokb_tipp_t'
-
     version column: 'VER_NBR'
-    // Consider --    id generator: 'hilo', params: [table: 'ole_e_res_rec_s', column: 'id', max_lo: 100]
-    // This works OK
-    // id generator:'assigned', column:'PKG_ID'
-    // seqhilo
-    // id generator: 'hilo', params: [table: 'ole_gokb_pkg_t', column: 'PKG_ID', max_lo: 100], column:'PKG_ID'
     id generator: 'hilo', params: [table: 'ole_gokb_tipp_s', column: 'ID', max_lo: 100], column:'TIPP_ID'
+    isbn column:'ISBN'
+    issn column:'ISSN'
+    eissn column:'EISSN'
+    doi column:'DOI'
+    pkg column:'GOKB_PKG_FK'
+    accessUrl column:'ACCESS_URL'
+    coverageStartDate column:'COV_START_DATE'
+    coverageStartVolume column:'COV_START_VOL'
+    coverageStartIssue column:'COV_START_ISS'
+    coverageEndDate column:'COV_END_DATE'
+    coverageEndVolume column:'COV_END_VOL'
+    coverageEndIssue column:'COV_END_ISS'
     createdDate column:'DATE_CREATED'
     lastModifiedDate column:'LAST_UPDATED'
   }
